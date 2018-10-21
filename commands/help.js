946b571b54a69ea3677526573d0f995af4b3d9fe
Slacker Company <38662305+MuulshCorp@users.noreply.github.com>
2018-10-21 12:03:53 +0200

    Commandes

--- commands/help.js
+++ commands/help.js
@@ -1,8 +1,10 @@
 const Discord = require("discord.js");
-module.exports.run = async (client, message, args) => {
+module.exports.run = async (client, message, args, command, config, fs) => {
 //mettre la commande ici
 if(command == "help") { 
-    return message.channel.send('›`'+config.prefix+'say <message>` \nPour utiliser cette commande il vous faut le rôle `'+config.role.staff+'`\n\n›`'+config.prefix+'prefix <nouveau préfixe>` \nPour utiliser cette commande il vous faut le rôle `'+config.role.staff+'`\n\n›`'+config.prefix+'edit <role> <nouveau nom du rôle>` \nPour modifier les rôles qu\'il faut avoir pour utiliser les commandes; pour utiliser cette commande il vous faut le rôle `'+config.role.staff+'`');
+    return message.channel.send('›`'+config.prefix+'say <message>` \nPour utiliser cette commande il vous faut le rôle `'+config.role.staff+
+    	'`\n\n›`'+config.prefix+'edit <role> <nouveau nom du rôle>` \nPour modifier les rôles qu\'il faut avoir pour utiliser les commandes; pour utiliser cette commande il vous faut le rôle `'+config.role.staff+
+    	'`');
 }
 //fin
 }
