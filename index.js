3c4eb7d1ff157ba1866bab418b68a60174b8d26d
Slacker Company <38662305+MuulshCorp@users.noreply.github.com>
2018-10-21 10:58:36 +0200

    Bot Pasteque

--- index.js
+++ index.js
@@ -2,59 +2,40 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require("fs");
 
+fs.readdir("./commands/", (err, files) => {
+
+  if(err) console.log(err);
+  let jsfile = files.filter(f => f.split(".").pop() === "js");
+  if(jsfile.length <= 0){
+    console.log("Couldn't find commands.");
+    return;
+  }
+
+  jsfile.forEach((f, i) =>{
+    let props = require(`./commands/${f}`);
+    console.log(`${f} loaded!`);
+    client.commands.set(props.help.name, props);
+  });
+});
 
 client.on("ready", () => {
   let rawdata = fs.readFileSync('config.json');  
   let config = JSON.parse(rawdata);
-  console.log('ready');
-  client.user.setActivity(config.prefix+`help`);
+  console.log('ready'+client.guilds.size+"serveurs | "+config.prefix+`help`);
+  client.user.setActivity(client.guilds.size+"serveurs | "+config.prefix+`help`, {type: "WATCHING"});
 });
 
 client.on("message", async message => {
-let rawdata = fs.readFileSync('config.json');  
-let config = JSON.parse(rawdata);
+let rawdconfig = fs.readFileSync('config.json');  
+let config = JSON.parse(rawconfig);
 const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
 const command = args.shift().toLowerCase();
 
 if(message.content.indexOf(config.prefix) !== 0) return;
 if (!message.content.startsWith(config.prefix) || message.author.bot) return;
 
-if(command == "say") {
-  	if(!message.member.roles.some(r=>[config.role.staff].includes(r.name)))
-  	return message.reply("Vous n'avez pas le rôle `"+config.role.staff+"` pour utiliser cette commande!");
-    
-    if (args[0] == "" || args[0] == null) {
-      return message.channel.send('Vous devez écrire un message !')
-    }
-
-    const sayMessage = args.join(" ");
-    message.delete().catch(O_o=>{}); 
-    return message.channel.send(sayMessage);
-}
-
-if(command == "prefix") {
-  	if(!message.member.roles.some(r=>[config.role.staff].includes(r.name)) )
-  	return message.reply("Vous n'avez pas le rôle `"+config.role.staff+"` pour utiliser cette commande!");
-
-    if (args[0] == "" || args[0] == null) {
-      return message.channel.send('Vous devez choisir un nouveau préfixe ! (le nouveau préfixe ne doit pas contenir d\'espace)')
-    }
-
-  	prefix = args[0];
-
-    var raw = { prefix: prefix, role: { say: config.role.say, staff: config.role.staff } };
-    let data = JSON.stringify(raw, null, 2);
-    fs.writeFileSync('config.json', data);
-
-    client.user.setActivity(prefix+`help`);
-    return message.channel.send('Le nouveau préfixe est `'+prefix+'` !').catch(O_o=>{});
-
-	
-}
-
-if(command == "help") { 
-	return message.channel.send('›`'+config.prefix+'say <message>` \nPour utiliser cette commande il vous faut le rôle `'+config.role.staff+'`\n\n›`'+config.prefix+'prefix <nouveau préfixe>` \nPour utiliser cette commande il vous faut le rôle `'+config.role.staff+'`\n\n›`'+config.prefix+'edit <role> <nouveau nom du rôle>` \nPour modifier les rôles qu\'il faut avoir pour utiliser les commandes; pour utiliser cette commande il vous faut le rôle `'+config.role.staff+'`');
-}
+  let commandfile = client.commands.get(cmd.slice(prefix.length));
+  if(commandfile) commandfile.run(client,message,args);
 
 });
 client.login(process.env.TOKEN);
